d1e87fb65a0aeebd885683932d35b9230ef796ac
dogebi <rainxlh@gmail.com>
2021-07-27 18:22:29 +0800

    gender add pets name

--- src/components/Contentes.js
+++ src/components/Contentes.js
@@ -1,28 +1,20 @@
-/*eslint-disable */
-
 import { useEffect, useState } from "react";
 
+/*eslint-disable */
 import React from "react";
+import ReactDOM from "react-dom";
 import axios from "axios";
 
 const arraySort = require("array-sort"); //other fangfa
 
 const Contents = () => {
-  //   let [글제목, 글제목변경] = useState(["gender", "name"]);
-
-  //   function 제목변경() {
-  //     let newArray = [...글제목]; //복사본 만들기..deep copy해서 만든다.
-  //     newArray[0] = "gender";
-  //     글제목변경(newArray);
-  //   }
-
   useEffect(() => {
     const fetchEvents = async () => {
       const res = await axios.get(
         "https://cors-anywhere.herokuapp.com/https://5c92dbfae7b1a00014078e61.mockapi.io/owners"
       );
       //console.log(res.data);
-      const userList = res.data;
+      var userList = res.data;
       console.log(arraySort(userList, "pets.name"));
 
       //console.log(arraySort(userList, "pets.name"));
@@ -50,37 +42,6 @@ const Contents = () => {
       // );
 
       // console.log(userList[userList.length - 1].pets);
-
-      var sortOption = {
-        name: true,
-        gender: true,
-        age: true,
-        pets: true,
-      };
-
-      function sort123(sortField) {
-        if (sortOption[sortField]) {
-          userList = userList.sort((a, b) =>
-            a[sortField] < b[sortField]
-              ? -1
-              : a[sortField] > b[sortField]
-              ? 1
-              : 0
-          );
-        } else {
-          userList = userList.sort((a, b) =>
-            a[sortField] < b[sortField]
-              ? 1
-              : a[sortField] > b[sortField]
-              ? -1
-              : 0
-          );
-        }
-
-        sortOption[sortField] = !sortOption[sortField];
-
-        renderTable(userList);
-      }
 
       function renderTable(data) {
         var oTable = document.querySelector("table>tbody");
@@ -115,15 +76,39 @@ const Contents = () => {
     fetchEvents();
   }, []);
 
+  var sortOption = {
+    name: true,
+    gender: true,
+    age: true,
+    pets: true,
+  };
+
+  function sort123(sortField) {
+    alert(sortField);
+    if (sortOption[sortField]) {
+      userList = userList.sort((a, b) =>
+        a[sortField] < b[sortField] ? -1 : a[sortField] > b[sortField] ? 1 : 0
+      );
+    } else {
+      userList = userList.sort((a, b) =>
+        a[sortField] < b[sortField] ? 1 : a[sortField] > b[sortField] ? -1 : 0
+      );
+    }
+
+    sortOption[sortField] = !sortOption[sortField];
+
+    renderTable(userList);
+  }
+
   return (
     <section>
-      <h5>
+      <p>
         need to write some code to consume the json and output a list of all the
         cats in alphabetical order under a heading of the gender of their owner.
-      </h5>
+      </p>
       <div className='container'>
         <div>
-          <table className='table table-bordered table-dark table-sm'>
+          <table className='table table-bordered table-dark table-hover table-sm'>
             <thead>
               <tr>
                 <td>GENDER</td>
